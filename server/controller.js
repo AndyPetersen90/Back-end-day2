4607e8873783a45cbf61402547f868dee577d762
Andrew Petersen <fespetersen1990@gmail.com>
2022-05-03 14:52:52 -0600

    Part 3.4 completed

--- server/controller.js
+++ server/controller.js
@@ -1,12 +1,18 @@
 
 const houses = require("./db.json");
 
+let globalId = 4
+
 module.exports = {
     getHouse: (req, res) => {
-
+        res.status(200).send(houses);
     },
     deleteHouse: (req, res) => {
-        
+        let index = houses.findIndex((house) => {
+            return house.id === +req.params.id;
+        })
+        houses.splice(index, 1);
+        res.status(200).send(houses);
     },
     createHouse: (req, res) => {
         
